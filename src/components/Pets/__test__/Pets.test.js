bdd6e750099e8e4bc73541f8846fde19469ba49b
Benjamin Lamboy <blamboy@catalyte.io>
2022-11-02 10:46:43 -0600

    changes test to have render pets in beforeEach call

--- src/components/Pets/__test__/Pets.test.js
+++ src/components/Pets/__test__/Pets.test.js
@@ -10,13 +10,14 @@ const server = setupServer(
   })
 );
 
+// eslint-disable-next-line testing-library/no-render-in-setup
+beforeEach(() => render(<Pets />));
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe("Pets", () => {
   test("should render the correct amount of cards", async () => {
-    render(<Pets />);
     const cards = await screen.findAllByRole("article");
     expect(cards.length).toBe(5);
   });
